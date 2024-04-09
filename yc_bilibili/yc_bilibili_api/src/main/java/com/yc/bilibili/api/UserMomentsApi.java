22835342a187b03e1d0a33dca64c33bf819bfc38
HashQ <1249023170@qq.com>
2024-04-09 15:26:27 +0800

    完善用户发布动态

--- yc_bilibili/yc_bilibili_api/src/main/java/com/yc/bilibili/api/UserMomentsApi.java
+++ yc_bilibili/yc_bilibili_api/src/main/java/com/yc/bilibili/api/UserMomentsApi.java
@@ -4,6 +4,8 @@ import com.yc.bilibili.api.support.UserSupport;
 import com.yc.bilibili.daomin.JsonResponse;
 import com.yc.bilibili.daomin.UserMoment;
 import com.yc.bilibili.service.UserMomentsService;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,7 +28,7 @@ public class UserMomentsApi {
      * 用户发布动态接口
      */
     @PostMapping("/user-moments")
-    public JsonResponse<String> addUserMoments(@RequestBody UserMoment userMoment){
+    public JsonResponse<String> addUserMoments(@RequestBody UserMoment userMoment) throws RemotingException, InterruptedException, MQClientException {
         Long userId = userSupport.getCurrentUserId();
         userMoment.setUserId(userId);
         userMomentsService.addUserMoments(userMoment);
--- yc_bilibili/yc_bilibili_service/src/main/java/com/yc/bilibili/service/UserMomentsService.java
+++ yc_bilibili/yc_bilibili_service/src/main/java/com/yc/bilibili/service/UserMomentsService.java
@@ -1,12 +1,20 @@
 package com.yc.bilibili.service;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.yc.bilibili.dao.UserMomentsDao;
 import com.yc.bilibili.daomin.UserMoment;
+import com.yc.bilibili.daomin.constant.UserMomentsConstant;
+import com.yc.bilibili.service.util.RocketMQUtil;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
@@ -18,8 +26,12 @@ public class UserMomentsService {
     @Autowired
     private ApplicationContext applicationContext;
 
-    public void addUserMoments(UserMoment userMoment) {
+    public void addUserMoments(UserMoment userMoment) throws RemotingException, InterruptedException, MQClientException {
         userMoment.setCreateTime(new Date());
         userMomentsDao.addUserMoments(userMoment);
+        //获取mq 生产者和消费者
+        DefaultMQProducer producer = (DefaultMQProducer)applicationContext.getBean("momentsProducer");
+        Message msg = new Message(UserMomentsConstant.TOPIC_MOMENTS, JSONObject.toJSONString(userMoment).getBytes(StandardCharsets.UTF_8));
+        RocketMQUtil.asynSendMsg(producer,msg);
     }
 }
