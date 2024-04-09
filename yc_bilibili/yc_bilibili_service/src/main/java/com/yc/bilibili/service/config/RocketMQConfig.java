d24d73fdd0c219b80e0107e53f46fabae780364e
HashQ <1249023170@qq.com>
2024-04-09 15:41:50 +0800

    RockerMQConfig改造

--- yc_bilibili/yc_bilibili_service/src/main/java/com/yc/bilibili/service/config/RocketMQConfig.java
+++ yc_bilibili/yc_bilibili_service/src/main/java/com/yc/bilibili/service/config/RocketMQConfig.java
@@ -1,6 +1,13 @@
 package com.yc.bilibili.service.config;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.JSONPResponseBodyAdvice;
+import com.yc.bilibili.daomin.UserFollowing;
+import com.yc.bilibili.daomin.UserMoment;
 import com.yc.bilibili.daomin.constant.UserMomentsConstant;
+import com.yc.bilibili.service.UserFollowingService;
+import io.netty.util.internal.StringUtil;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
@@ -16,6 +23,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,6 +38,11 @@ public class RocketMQConfig {
 
     @Autowired
     private RedisTemplate<String,String> redisTemplate;
+
+
+    @Autowired
+    private UserFollowingService userFollowingService;
+
 
     /**
      * 生产者
@@ -61,8 +74,29 @@ public class RocketMQConfig {
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
-                for (Message msg : msgs) {
-                    System.out.println(msg);
+                //用户动态实体发送到mq  获取消息处理
+                Message msg = msgs.get(0);
+                if(msg == null ){
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                }
+                String bodyStr = new String(msg.getBody());
+                UserMoment userMoment = JSONObject.toJavaObject(JSONObject.parseObject(bodyStr),UserMoment.class);
+                Long userId = userMoment.getUserId();
+                //获取用户的粉丝
+                List<UserFollowing> fanList = userFollowingService.getUserFans(userId);
+                for (UserFollowing fan : fanList) {
+                    //消息放到redis  那个用户订阅的内容
+                    String key = "subscribed-"+ fan.getUserId();
+                    String subscribedListStr = redisTemplate.opsForValue().get(key);
+                    List<UserMoment> subscribedList ;
+                    if(StringUtil.isNullOrEmpty(subscribedListStr)){
+                        subscribedList = new ArrayList<>();
+                    }else {
+                        // 字符串转换成实体
+                        subscribedList = JSONArray.parseArray(subscribedListStr,UserMoment.class);
+                    }
+                    subscribedList.add(userMoment);
+                    redisTemplate.opsForValue().set(key,JSONObject.toJSONString(subscribedList));
                 }
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
